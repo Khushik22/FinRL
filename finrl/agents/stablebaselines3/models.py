b06654b0eebf27b4091c4a6d031bb841d14bd56d
Khushik22 <55204081+Khushik22@users.noreply.github.com>
2022-05-05 19:51:57 +0530

    Update models.py

--- finrl/agents/stablebaselines3/models.py
+++ finrl/agents/stablebaselines3/models.py
@@ -5,7 +5,7 @@ import time
 import numpy as np
 import pandas as pd
 import sys
-sys.path.insert(0, '/home/khushi/Desktop/FinRL/finrl/agents/stablebaselines3/stable_base_lines3/stable_baselines3/ppo')
+sys.path.insert(0, '/Khushik22/FinRL/finrl/agents/stablebaselines3/stable_base_lines3/stable_baselines3/ppo')
 from ppo import PPO
 from stable_baselines3 import A2C, DDPG, SAC, TD3
 from stable_baselines3.common.callbacks import BaseCallback
